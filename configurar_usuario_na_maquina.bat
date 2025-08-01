f167d5d9a9f32eee0e5b5b4e7afa80006989e297
Luizitoox <usoparaetec@gmail.com>
2025-07-31 22:32:42 -0300

    Update configurar_usuario_na_maquina.bat

--- configurar_usuario_na_maquina.bat
+++ configurar_usuario_na_maquina.bat
@@ -7,8 +7,8 @@ git config --global --unset-all user.name
 
 echo --- Configurando Usuario GitHUB ----
 
-git config --global user.name "Camilla Vitoria"
-git config --global user.email camillav09@gmail.com
+git config --global user.name "Luiz Batista"
+git config --global user.email usoparaetec@gmail.com
 echo --- Mostrando a configuração atual do Github no computador
 git config --list
 echo --- Fim ---
