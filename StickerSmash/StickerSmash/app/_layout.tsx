7c833797fb477ccd71075fc27b03715e097bf13c
Luiz Batista <usoparaetec@gmail.com>
2025-10-24 21:56:09 -0300

    StickerSmash

---
+++ StickerSmash/StickerSmash/app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index"
+      options={{
+        headerTitle: "Sticker Smash",
+      }}
+      />
+      
+      <Stack.Screen name="about"
+      options={{
+        headerTitle: "About",
+      }}
+      />
+    </Stack>
+  );
+}
