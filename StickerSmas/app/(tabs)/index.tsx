1d53c93da858a0da14d9740ae39e9e2f06d52a1e
Luiz Batista <usoparaetec@gmail.com>
2025-10-30 22:21:53 -0300

    StickerSmash

---
+++ StickerSmas/app/(tabs)/index.tsx
@@ -0,0 +1,38 @@
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import { StyleSheet, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1C1C1C',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  imageContainer: {
+    flex: 1,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
