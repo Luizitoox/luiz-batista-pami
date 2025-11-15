6c82c09dfa2a2ac3297b650b99666b4545b84608
Luiz Batista <usoparaetec@gmail.com>
2025-11-15 15:37:47 -0300

    StickerSmahs completo

--- StickerSmash/components/CircleButton.tsx
+++ StickerSmash/components/CircleButton.tsx
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
         height: 84,
         marginHorizontal: 60,
         borderWidth: 4,
-        borderColor: '#ffd33d',
+        borderColor: '#FF4500',
         borderRadius: 42,
         padding: 3,
     },
